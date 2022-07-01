a4ae7a995b7bb97cdd7f63302ccba78a1bdbe31b
Farhan <MNURFARHAN13.20@ichat.sp.edu.sg>
2022-07-02 00:19:43 +0800

    Update customerProfile.js

--- Frontend/public/assets/js/customerProfile.js
+++ Frontend/public/assets/js/customerProfile.js
@@ -9,7 +9,6 @@ const backEndUrl = 'http://localhost:5000';
 // const frontEndUrl = 'https://moc-fa.herokuapp.com';
 // const backEndUrl = 'https://moc-ba.herokuapp.com';
 const tmpToken = JSON.parse(localStorage.getItem('token'));
-
 if (tmpToken === null) {
   window.location.replace(`${frontEndUrl}/unAuthorize`);
 }
